4044ff275f8d3f0bdff8a55ba504a42975951f3a
Zain Awais <zain.awais@hotmail.com>
2023-06-02 20:25:18 +0500

    improve routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
-  get 'movies/index'
-  get 'movies/now_playing'
-  get 'movies/top_rated'
-  get 'movies/popular'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  
+  resources :movies, only: [:index] do
+    collection do
+      get :now_playing
+      get :top_rated
+      get :popular
+      get :genre
+    end
+  end
+  
+  root 'movies#index'
 end
